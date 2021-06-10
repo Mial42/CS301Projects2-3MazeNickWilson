304914d781d69ca13f3d1cd298ef1ec08ecfa2c8
nbwilson <nbwilson@email.wm.edu>
2021-06-10 12:52:35 -0400

    Release 0.2
    Created MazeFactoryTest.java

---
+++ MazeByNickWilson/test/generation/MazeFactoryTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package generation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nicholas Wilson
+ *
+ */
+class MazeFactoryTest {
+	private MazeFactory myMazeFactory; //MazeFactory to run tests on
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		myMazeFactory = new MazeFactory(); //Instantiate MyMazeFactory
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
